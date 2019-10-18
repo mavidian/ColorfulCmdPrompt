869d1fb159758315f241b12bcd8fa4d235653fb0
Wojtek Dziegielewski <wojtekd@mavidian.com>
2019-10-17 23:42:47 -0400

    Initial 3 batch files.

---
+++ FirstTask.bat
@@ -0,0 +1,4 @@
+REM This is a simulated output from an executable program, e.g. a .NET console application.
+echo The first task is starting.
+echo There were 500 records processed with no errors.
+echo The first task has completed processing in 0.001s.
---
+++ SecondTask.bat
@@ -0,0 +1,5 @@
+REM This is a simulated output from an executable program, e.g. a .NET console application.
+echo The second task is starting.
+echo Error occurred in record# 344: Invalid record type.
+echo There were 500 records processed with 1 error.
+echo The second task has completed processing in 0.003s.
